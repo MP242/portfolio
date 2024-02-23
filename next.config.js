b7f8b29368701028f080d885b6c1ac9624aedb29
MP242 <marc.pollet@epitech.eu>
2024-02-23 19:51:43 +0400

    clean next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,4 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  env: {
-    // REACT_APP_API_URL:'http://127.0.0.1:8000',
-    REACT_APP_API_URL:'https://rag-server-1b54e9d61568.herokuapp.com',
-  },
 }
