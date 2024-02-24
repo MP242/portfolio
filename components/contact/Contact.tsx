f304a92fe492359cb2eefd33ea7291012e53eb42
MP242 <marc.pollet@epitech.eu>
2024-02-24 11:24:06 +0400

    modif mobile

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -11,17 +11,47 @@ const Contact = () => {
         <h4>{t([`common`, `contactSection.title`])}</h4>
         <div className={style.container__content}>
           <p className={style.container__content__text}>
-          {t([`common`, `contactSection.getInTouch`])}
+            {t([`common`, `contactSection.getInTouch`])}
           </p>
-          <div className={style.container__content__email}>
-            <a href="mailto:marc.pollet242@gmail.com" aria-label="Email de Marc Pollet">📩</a>
-            <div className={style.container__content__email__text}>
-              <h4>{t([`common`, `contactSection.email`])}</h4>
-              <p>{t([`common`, `contactSection.myEmail`])}</p>
+          <div className={style.container__content__contactContainer}>
+            <div className={style.container__content__contactContainer__contact}>
+              <a
+                className={style.container__content__contactContainer__contact__icone}
+                href="mailto:marc.pollet242@gmail.com"
+                aria-label="Email de Marc Pollet"
+              >
+                📩
+              </a>
+              <div className={style.container__content__contactContainer__contact__text}>
+                <h4>{t([`common`, `contactSection.email`])}</h4>
+                {/* <p>{t([`common`, `contactSection.myEmail`])}</p> */}
+                <a
+                  className={style.container__content__contactContainer__contact__text__link}
+                  href="mailto:marc.pollet242@gmail.com"
+                aria-label="Email de Marc Pollet"
+                >
+                  {t([`common`, `contactSection.myEmail`])}
+                </a>
+              </div>
             </div>
-            <div className={style.container__content__email__text}>
-              <h4>{t([`common`, `contactSection.phone`])}</h4>
-              <p>{t([`common`, `contactSection.myPhone`])}</p>
+            <div className={style.container__content__contactContainer__contact}>
+              <a
+                className={style.container__content__contactContainer__contact__icone}
+                href="tel:+262693515332"
+                aria-label="Telephone de Marc Pollet"
+              >
+                📞
+              </a>
+              <div className={style.container__content__contactContainer__contact__text}>
+                <h4>{t([`common`, `contactSection.phone`])}</h4>
+                <a
+                  className={style.container__content__contactContainer__contact__text__link}
+                  href="tel:+262693515332"
+                >
+                  {t([`common`, `contactSection.myPhone`])}
+                </a>
+                {/* <p>{t([`common`, `contactSection.myPhone`])}</p> */}
+              </div>
             </div>
           </div>
         </div>
